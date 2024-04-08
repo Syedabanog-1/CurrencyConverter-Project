5e876095e235761bbcab05a3e8cfcdcf71312bc8
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-08 15:33:24 +0500

    Add files via upload

---
+++ currency_Convert.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    PKR: 280,
+    AFG: 71,
+    CAD: 204,
+    DIN: 40,
+    EUR: 0.91,
+    INR: 74.57
+};
+let user_Answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: " Enter your Currency From",
+        type: "list",
+        choices: ["USD", "PKR", "AFG", "CAD", "DIN", "EUR", "INR"]
+    },
+    {
+        name: "to",
+        message: " Enter your Currency To",
+        type: "list",
+        choices: ["USD", "PKR", "AFG", "CAD", "DIN", "EUR", "INR"]
+    },
+    {
+        name: "amount",
+        message: " Enter your Amount",
+        type: "number",
+    },
+]);
+let fromAmount = currency[user_Answer.from];
+let toAmount = currency[user_Answer.to];
+let amount = user_Answer.amount;
+let baseAmount = amount / fromAmount;
+let convertAmount = baseAmount * toAmount;
+//console.log("Covert From " , fromAmount , " Convrt To" , toAmount, "Conversion Amount is", convertAmount);
+console.log("Conversion Amount is", Math.round(convertAmount));
